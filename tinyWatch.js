37be8b5bc7fb6f791422f8e8ee5edfcf009c76e9
demaiodev <fml1041@gmail.com>
2021-11-21 00:42:57 -0500

    fix tinyWatcher

--- tinyWatch.js
+++ tinyWatch.js
@@ -1,52 +1,37 @@
 const axios = require('axios');
 const nodemailer = require('nodemailer');
-const fs = require('fs');
 require('dotenv').config();
 
 const spacer = '\n---------------------------------------';
 const to =
   'fml1041@gmail.com, hilbertwilliam@gmail.com, tylerlangties@gmail.com';
 let counter = 1;
+let previous = { id: 0 };
 
 function text(latest) {
   return `💰 New ASA listed on TinyChart! 💰 \n
-📊 Go directly to Tinyman: https://app.tinyman.org/#/swap?asset_in=0&asset_out=${
-    latest.id
-  } \n
-📈 Click to see the TinyChart: https://tinychart.org/asset/${latest.id} \n
-📋 Details:\n
-${JSON.stringify(latest, null, 2)} \n`;
+📊 Go directly to Tinyman: https://app.tinyman.org/#/swap?asset_in=0&asset_out=${latest.id} \n
+📈 Click to see the TinyChart: https://tinychart.org/asset/${latest.id} \n`;
 }
 
 function getLatest() {
-  let time = new Date().toLocaleString();
+  const time = new Date().toLocaleString();
   console.log(`Attempt #${counter} - ${time} ${spacer}`);
   console.log('🚚 Getting latest ASA listings... 🚚');
   axios
     .get('https://api.v3.tinychart.org/assets/')
     .then(({ data }) => {
-      const latest = data
+      const current = data
         .sort((a, b) => new Date(b.created) - new Date(a.created))
         .slice(0, 1)[0];
-      fs.readFile('./latest.json', 'utf8', (err, data) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        console.log('🔍 Checking the latest... 🔍');
-        if (data) {
-          console.log(text(latest));
-          sendEmail(latest, time);
-        } else {
-          console.log(`😩😓 No luck! 😓😩 ${spacer}\n`);
-        }
-      });
-      fs.writeFile('./latest.json', JSON.stringify(latest), (err) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-      });
+      console.log('🔍 Checking the latest... 🔍');
+      if (current.id !== previous.id) {
+        console.log(text(current));
+        sendEmail(current, time);
+      } else {
+        console.log(`😩😓 No luck! 😓😩 ${spacer}\n`);
+      }
+      previous = current;
       counter++;
     })
     .catch((error) => {
@@ -77,4 +62,4 @@ function sendEmail(latest, time) {
 
 setInterval(() => {
   getLatest();
-}, 120000);
+}, 60000);
